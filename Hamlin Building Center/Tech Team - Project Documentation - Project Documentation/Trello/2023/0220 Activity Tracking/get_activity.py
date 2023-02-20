61bd368064ab19faba520ff486d80d33aca55c44
Reid.Homola <reidghomola@gmail.com>
2023-02-20 14:58:24 -0600

    some stuff occurred!

--- Hamlin Building Center/Tech Team - Project Documentation - Project Documentation/Trello/2023/0220 Activity Tracking/get_activity.py
+++ Hamlin Building Center/Tech Team - Project Documentation - Project Documentation/Trello/2023/0220 Activity Tracking/get_activity.py
@@ -4,40 +4,41 @@ import os
 
 KEY = '135e060c189bf0f4d6b8a021b3804e1e'
 TOKEN = 'ATTAc7971e017e294d0507b749e386b16a9e364b1cc93690191afad1cf2ea061d6f608A7C79C'
-board_ids = [
-    '5b000bde9bb5dee945febd49', # Joe Homola
-    '63c1cc3045fcaf0016463c46', # Ruben Lopez
-    '5a84a60c863c552358f519ff', # Cameron Larson
-    '5a958b46beac45adb69d8e04', # Erin Thiex
-    '5a84a5f52fe5a3670b22da17', # Jenny Jasper
-    '5a84a522c6c12d4e35689369', # Kamden Homola
-    '62e14eb9ddd9da37a7a0b32e', # Lars Williamson
-    '5bfd7c1bd97b0a09ade4e5b6', # Lason Wilen
-    '63eabae597b4646ccc4226d1'  # Shane VanDamme
-]
+board_ids = {
+    '5b000bde9bb5dee945febd49': 'Joe Homola',
+    '63c1cc3045fcaf0016463c46': 'Ruben Lopez',
+    '5a84a60c863c552358f519ff': 'Cameron Larson',
+    '5a958b46beac45adb69d8e04': 'Erin Thiex',
+    '5a84a5f52fe5a3670b22da17': 'Jenny Jasper',
+    '5a84a522c6c12d4e35689369': 'Kamden Homola',
+    '62e14eb9ddd9da37a7a0b32e': 'Lars Williamson',
+    '5bfd7c1bd97b0a09ade4e5b6': 'Lason Wilen',
+    '63eabae597b4646ccc4226d1': 'Shane VanDamme'
+}
 
 with open('output.json', 'w') as f:
     f.write('')
 
 for board_id in board_ids:
-    print(board_id)
-    headers = {
-        'Accept': 'application/json'
-    }
+    print(board_ids.get(board_id))
 
+    
     query = {
         'key': KEY,
         'token': TOKEN,
-        'fields': 'name,labels,url,idShort,idList,dateLastActivity',
-        'since': '2023-02-13'
+        'filter': 'updateCard'
     }
 
     response = requests.request(
         'GET',
         'https://api.trello.com/1/board/' + board_id + '/actions',
-        headers=headers,
+        # headers=headers,
         params=query
     )
+
+    json_data = json.loads(response.text)
+    for j in json_data:
+        print(j)
 
     with open('output.json', 'a') as f:
         if response.text != 'The requested resource was not found.':
